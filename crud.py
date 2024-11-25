ec9945ba31b187d8d12512df064c9b6b5d8d45a9
Matthew Boubin <mjb9353@nyu.edu>
2024-11-25 15:52:41 -0500

    added basic, skeletal crut functionality

---
+++ crud.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import pickle, time, os
+from flask_apscheduler import APScheduler
+
+app = Flask(__name__)
+
+global dictionary
+dictionary = {}
+file = open("dictionary_file.pkl", 'wb')
+pickle.dump(dictionary, file)
+file.close()
+
+execute_clear_dictionary = APScheduler()
+
+# Homepage
+@app.route("/")
+def crud_page():
+    return render_template("/choose_crud_command.html")
+
+def clear_dictionary():
+    os.remove("dictionary_file.pkl")
+    dictionary = {}
+    file = open("dictionary_file.pkl", 'wb')
+    pickle.dump(dictionary, file)
+    file.close()
+
+if __name__== '__main__':
+    execute_clear_dictionary.add_job(id = 'Scheduled Task', func=clear_dictionary, trigger="interval", seconds=300)
+    execute_clear_dictionary.start()
+    app.run(host="0.0.0.0", debug=True, port=8888)
