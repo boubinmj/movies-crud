c09fa237ddd74a5f1bdd6c932e11088253ae14d0
Matt Boubin <mjb9353@nyu.edu>
2024-12-06 21:10:20 -0500

    added get request functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import pickle, time, os
+import pickle, time, os, json
 from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
@@ -29,14 +29,20 @@ def home():
 def contact():
     return render_template("/contact.html")
 
+@app.route('/get_page')
+def get_page():
+    with open('db/data.json', 'r') as f:
+        resp = json.load(f)
+        return json.dumps(resp, indent=4)
+
 def clear_dictionary():
     os.remove("dictionary_file.pkl")
-    dictionary = {}
+    dictionary = json.loads("data.json")
     file = open("dictionary_file.pkl", 'wb')
     pickle.dump(dictionary, file)
     file.close()
 
 if __name__== '__main__':
-    execute_clear_dictionary.add_job(id = 'Scheduled Task', func=clear_dictionary, trigger="interval", seconds=300)
-    execute_clear_dictionary.start()
+    #execute_clear_dictionary.add_job(id = 'Scheduled Task', func=clear_dictionary, trigger="interval", seconds=10)
+    #execute_clear_dictionary.start()
     app.run(host="0.0.0.0", debug=True, port=8888)
